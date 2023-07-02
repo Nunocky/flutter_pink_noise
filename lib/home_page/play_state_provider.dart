6527bf95f2b47a68e7fe666554365d3ba87ecf6f
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2023-07-02 13:49:28 +0900

    音の再生
    Android Emulatorだと音が途切れがち。 iOSシミュレータだと大丈夫

--- lib/home_page/play_state_provider.dart
+++ lib/home_page/play_state_provider.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../logger.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_session/audio_session.dart';
+
+late AudioPlayer _player;
 
 // 再生状態を監視し、バックグラウンドで音を鳴らす StateNotifier を作る
 // それを使って、ボタンを押したら音を鳴らす
@@ -8,17 +12,54 @@ final playStateProvider = StateNotifierProvider<PlayStateNotifier, bool>((ref) {
 });
 
 class PlayStateNotifier extends StateNotifier<bool> {
-  PlayStateNotifier(super.state);
+  PlayStateNotifier(super.state) {
+    _player = AudioPlayer();
 
-  void toggle() {
+    _player.setLoopMode(LoopMode.all);
+
+    _player.playbackEventStream.listen(
+      (event) {
+        // // 再生中の時間を取得
+        // final position = event.processingState == ProcessingState.idle
+        //     ? Duration.zero
+        //     : event.position;
+        // logger.d("再生中の時間: $position");
+      },
+      onDone: () {
+        // _player.seek(Duration.zero);
+      },
+      onError: (Object e, StackTrace stackTrace) {
+        state = false;
+        _player.stop();
+      },
+    );
+  }
+
+  void toggle() async {
     state = !state;
 
     if (state) {
-      // 音を鳴らす
-      logger.d("TODO: 音を鳴らす");
+      final data = await rootBundle.load('assets/audio/pink_noise.mp3');
+
+      // 曲間で音が一瞬途切れてしまうのをどうにかしたい
+      _player.setAudioSource(
+          ConcatenatingAudioSource(
+            useLazyPreparation: true,
+            children: [
+              AudioSource.uri(Uri.dataFromBytes(Uint8List.view(data.buffer))),
+              // AudioSource.uri(Uri.dataFromBytes(Uint8List.view(data.buffer))),
+            ],
+          ),
+        initialPosition: Duration.zero,
+        preload: false,
+      );
+
+      final session = await AudioSession.instance;
+      await session.configure(const AudioSessionConfiguration.music());
+
+      await _player.play();
     } else {
-      // 音を止める
-      logger.d("TODO: 音を止める");
+      await _player.stop();
     }
   }
 }
