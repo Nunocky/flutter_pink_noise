d6d865d0268fed764738193b3bae870e37fcde64
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2023-07-13 08:55:51 +0900

    TimerSection追加

--- lib/home_page/my_home_page.dart
+++ lib/home_page/my_home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pink_noise/home_page/timer_section.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import './play_state_provider.dart';
+
 import './play_button.dart';
+import './play_state_provider.dart';
 
 /// ホームページ
 class MyHomePage extends ConsumerWidget {
@@ -10,21 +12,54 @@ class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final bool isPlaying = ref.watch(playStateProvider);
+    final remainingSecond = 777; // TODO(nunokawa) 再生タイマーのプロバイダを作る
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Pink Noise"),
       ),
-      body: Center(
-        child: PlayButton(
-          isPlaying: isPlaying,
-          onClicked: () {
-            // 再生状態を反転させる
-            ref.read(playStateProvider.notifier).toggle();
-          },
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TimerSection(
+                  isPlaying: isPlaying,
+                  remainingSeconds: remainingSecond,
+                  onTimerSetButtonClicked: _onTimerSetButtonClicked,
+                  onTimerOffButtonClicked: _onTimerOffButtonClicked,
+                  onTimerOnButtonClicked: _onTimerOnButtonClicked,
+                ),
+              ],
+            ),
+          ),
+          PlayButton(
+            isPlaying: isPlaying,
+            onClicked: () {
+              // 再生状態を反転させる
+              ref.read(playStateProvider.notifier).toggle();
+            },
+          ),
+          const Spacer(),
+        ],
       ),
     );
+  }
+
+  /// タイマー時間変更ボタンをタップ
+  void _onTimerSetButtonClicked() {
+    // TODO(nunokawa) IMPLEMENT THIS
+  }
+
+  /// タイマー開始ボタンをタップ
+  void _onTimerOnButtonClicked() {
+    // TODO(nunokawa) IMPLEMENT THIS
+  }
+
+  /// タイマー停止ボタンをタップ
+  void _onTimerOffButtonClicked() {
+    // TODO(nunokawa) IMPLEMENT THIS
   }
 }
